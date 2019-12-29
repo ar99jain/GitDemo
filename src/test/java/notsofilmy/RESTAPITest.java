9b57d8b7054a14d8674aa792313671b6962ba76a
Asus <ar99jain@gmail.com>
2019-12-29 14:21:17 +0530

    this is pune time zone guy

--- src/test/java/notsofilmy/RESTAPITest.java
+++ src/test/java/notsofilmy/RESTAPITest.java
@@ -7,6 +7,8 @@ public class RESTAPITest {
 	@Test
 	public void Postjira() {
 		System.out.println("Postjira");
+		System.out.println("Postjira2");
+		System.out.println("Postjira3");
 	}
 	@Test
 	public void DeleteTwitter() {
